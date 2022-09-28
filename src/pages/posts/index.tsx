5f0c9838cee1dd7e8edaf132a20b1ead0c5da887
Bernardo Muller <bernardo.muller.bm@gmail.com>
2022-09-28 10:28:33 -0300

    Mapping posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,9 +1,20 @@
-import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { getPrismicClient } from '../../services/prismic'
 import styles from './styles.module.scss'
+import { RichText } from 'prismic-dom'
 
-export default function Posts() {
+type Post = {
+  slug: string
+  title: string
+  excerpt: string
+  updatedAt: string
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -12,41 +23,39 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href='#'>
-            <time>10 de setembro de 2022</time>
-            <strong>Creating a new way to Learn!</strong>
-            <p>In this tutorial you will learn everything you need to know to learn even more!!!</p>
-          </a>
-          <a href='#'>
-            <time>10 de setembro de 2022</time>
-            <strong>Creating a new way to Learn!</strong>
-            <p>In this tutorial you will learn everything you need to know to learn even more!!!</p>
-          </a>
-          <a href='#'>
-            <time>10 de setembro de 2022</time>
-            <strong>Creating a new way to Learn!</strong>
-            <p>In this tutorial you will learn everything you need to know to learn even more!!!</p>
-          </a>
-          <a href='#'>
-            <time>10 de setembro de 2022</time>
-            <strong>Creating a new way to Learn!</strong>
-            <p>In this tutorial you will learn everything you need to know to learn even more!!!</p>
-          </a>
+          {posts.map(post => (
+            <a key={post.slug} href='#'>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   )
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const prismic = getPrismicClient()
 
-  const posts = await prismic.getByType("post", {
+  const response = await prismic.getByType("post", {
     pageSize: 100,
   });
 
-  console.log(posts);
-  
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      })
+    }
+  })
+
   return {
     props: { posts },
   };
