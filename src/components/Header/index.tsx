7ca348dcb1d46fdcae2a828cf7ab5986ed785eff
Bernardo Muller <bernardo.muller.bm@gmail.com>
2022-09-29 10:58:50 -0300

    Fix Nav Active Element / Fix Post Paragraph

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { ActiveLink } from "../ActiveLink";
 import { SignInButton } from "../SignInButton";
 
 import styles from './styles.module.scss'
@@ -7,14 +8,17 @@ export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <Image src="/images/logo.svg" alt="ig.news" width="108.45" height="30.27"/>
+        <Image src="/images/logo.svg" alt="ig.news" width="108.45" height="30.27" />
         <nav>
-          <a href="" className={styles.active}>Home</a>
-          <a href="">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
-
         <SignInButton />
       </div>
-    </header>
+    </header >
   )
 }
